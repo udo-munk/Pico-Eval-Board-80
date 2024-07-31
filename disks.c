a151d006e087d067671437353f6794ee8d496166
Udo Munk <udo.munk@freenet.de>
2024-07-31 17:01:08 +0200

    shorter LED times

--- disks.c
+++ disks.c
@@ -280,7 +280,7 @@ BYTE read_sec(int drive, int track, int sector, WORD addr)
 		stat = FDC_STAT_READ;
 	}
 
-	sleep_ms(1);
+	sleep_us(300);
 	put_pixel(0x000000); /* LED off */
 	return stat;
 }
@@ -317,7 +317,7 @@ BYTE write_sec(int drive, int track, int sector, WORD addr)
 		stat = FDC_STAT_WRITE;
 	}
 
-	sleep_ms(1);
+	sleep_us(300);
 	put_pixel(0x000000); /* LED off */
 	return stat;
 }
