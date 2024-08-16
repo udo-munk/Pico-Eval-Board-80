6d5c7d5ec744d09f086a8cd02ed11f4bb13a7061
Udo Munk <udo.munk@freenet.de>
2024-08-16 18:03:14 +0200

    cleanup

--- lcd/font/fonts.h
+++ lcd/font/fonts.h
@@ -38,19 +38,13 @@
 #ifndef __FONTS_H
 #define __FONTS_H
 
-/* Max size of bitmap will based on a font24 (17x24) */
-#define MAX_HEIGHT_FONT 24
-#define MAX_WIDTH_FONT	17
-#define OFFSET_BITMAP	54
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <stdint.h>
 
-typedef struct _tFont
-{    
+typedef struct _tFont {    
 	const uint8_t *table;
 	uint16_t Width;
 	uint16_t Height;
