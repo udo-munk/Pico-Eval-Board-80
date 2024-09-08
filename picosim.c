0749ebecf7db982eee828bde5ca351eeb76bdef7
Udo Munk <udo.munk@freenet.de>
2024-09-08 16:13:05 +0200

    adjust to z80pack changes

--- picosim.c
+++ picosim.c
@@ -74,7 +74,7 @@ void tud_cdc_send_break_cb(uint8_t itf, uint16_t duration_ms)
 	UNUSED(duration_ms);
 
 	cpu_error = USERINT;
-	cpu_state = STOPPED;
+	cpu_state = ST_STOPPED;
 }
 #endif
 
@@ -88,7 +88,7 @@ void gpio_callback(uint gpio, uint32_t events)
 	UNUSED(events);
 
 	cpu_error = USERINT;
-	cpu_state = STOPPED;
+	cpu_state = ST_STOPPED;
 }
 
 int main(void)
