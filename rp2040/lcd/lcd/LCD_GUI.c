8a38f8b93f8e2ec4e6b153d2a67a5e6d167f7899
Udo Munk <udo.munk@freenet.de>
2024-10-01 12:24:12 +0200

    fix time update, we sometimes miss 0 seconds

--- rp2040/lcd/lcd/LCD_GUI.c
+++ rp2040/lcd/lcd/LCD_GUI.c
@@ -524,28 +524,28 @@ void GUI_Showtime(POINT Xstart, POINT Ystart, POINT Xend, POINT Yend,
 	POINT Dy = Yend - Ystart;	// determine the font size
 	Font = GUI_GetFontSize(Dx, Dy);
 
-	if ((pTime->Min == 0) && (pTime->Sec == 0)) {
+	if ((pTime->Min == 0) && (pTime->Sec <= 1)) {
 		// 00:00:00
 		GUI_DrawRectangle(Xstart, Ystart, Xend, Yend,
 				  FONT_BACKGROUND, DRAW_FULL, DOT_PIXEL_1X1);
 		goto done;
 	}
 
-	if (((pTime->Min % 10) == 0) && (pTime->Sec == 0)) {
+	if (((pTime->Min % 10) == 0) && (pTime->Sec <= 1)) {
 		// xx:00:00
 		GUI_DrawRectangle(Xstart + Dx * 2, Ystart, Xend, Yend,
 				  FONT_BACKGROUND, DRAW_FULL, DOT_PIXEL_1X1);
 		goto done;
 	}
 
-	if (pTime->Sec == 0) {
+	if (pTime->Sec <= 1) {
 		// xx:x0:00
 		GUI_DrawRectangle(Xstart + Dx * 3, Ystart, Xend, Yend,
 				  FONT_BACKGROUND, DRAW_FULL, DOT_PIXEL_1X1);
 		goto done;
 	}
 
-	if ((pTime->Sec % 10) == 0) {
+	if ((pTime->Sec % 10) <= 1) {
 		// xx:xx:00
 		GUI_DrawRectangle(Xstart + Dx * 5, Ystart, Xend, Yend,
 				  FONT_BACKGROUND, DRAW_FULL, DOT_PIXEL_1X1);
