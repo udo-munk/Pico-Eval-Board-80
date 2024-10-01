b3c08c89c44acf1edf851e0658d0af98b03e4917
Udo Munk <udo.munk@freenet.de>
2024-10-01 17:14:02 +0200

    fix typo

--- rp2350/lcd.c
+++ rp2350/lcd.c
@@ -109,7 +109,7 @@ static void lcd_show_time(void)
 	dt.Sec = t->tm_sec;
 	GUI_Showtime(85, 10, 240, 35, &dt, BLUE);
 
-	/* update temerature */
+	/* update temperature */
 	GUI_DrawRectangle(423, 10, 457, 34, FONT_BACKGROUND,
 			  DRAW_FULL, DOT_PIXEL_1X1);
 	GUI_DrawRectangle(372, 10, 406, 34, FONT_BACKGROUND,
