d83c55dd659b04a77012b1e7b0e5157c92b33cc5
Udo Munk <udo.munk@freenet.de>
2024-10-02 08:49:16 +0200

    enable default UART

--- rp2040/picosim.c
+++ rp2040/picosim.c
@@ -133,9 +133,22 @@ int main(void)
 	put_pixel(rgb); /* red */
 
 	/* when using USB UART wait until it is connected */
+	/* but also get out if there is input at default UART */
+#if LIB_PICO_STDIO_UART
+	uart_inst_t *my_uart = uart_default;
+	/* destroy random input from UART after activation */
+	if (uart_is_readable(my_uart))
+		getchar();
+#endif
 #if LIB_PICO_STDIO_USB || LIB_STDIO_MSC_USB
 	lcd_wait_term();
 	while (!tud_cdc_connected()) {
+#if LIB_PICO_STDIO_UART
+		if (uart_is_readable(my_uart)) {
+			getchar();
+			break;
+		}
+#endif
 		rgb = rgb - 0x000100;	/* while waiting make */
 		if (rgb == 0)		/* RGB LED fading */
 			rgb = 0x005500;
