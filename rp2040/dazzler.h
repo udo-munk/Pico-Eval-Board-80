82554a6bda84beea08f8eeb78c84a8fbb45a0590
Udo Munk <udo.munk@freenet.de>
2024-10-08 17:26:52 +0200

    add dazzler

---
+++ rp2040/dazzler.h
@@ -0,0 +1,17 @@
+/*
+ * Emulation of Cromemco Dazzler on the RP2040-GEEK LCD
+ *
+ * Copyright (C) 2015-2024 by Udo Munk
+ * Copyright (C) 2024 by Thomas Eberhardt
+ */
+
+#ifndef DAZZLER_INC
+#define DAZZLER_INC
+
+#include "sim.h"
+#include "simdefs.h"
+
+extern void dazzler_ctl_out(BYTE data), dazzler_format_out(BYTE data);
+extern BYTE dazzler_flags_in(void);
+
+#endif /* !DAZZLER_INC */
