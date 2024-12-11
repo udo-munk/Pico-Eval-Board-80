a240b54afa66a4e25945ffd98458bc2fbd7383f0
Udo Munk <udo.munk@freenet.de>
2024-12-11 13:31:48 +0100

    refactor RP2040 and RP2350 into single source tree

--- srcsim/simcfg.c
+++ srcsim/simcfg.c
@@ -83,7 +83,7 @@ static int get_int(const char *prompt, const char *hint,
  */
 void config(void)
 {
-	const char *cfg = "/CONF80/EVA2040.DAT";
+	const char *cfg = "/CONF80/" CONF_FILE;
 	const char *cpath = "/CODE80";
 	const char *cext = "*.BIN";
 	const char *dpath = "/DISKS80";
